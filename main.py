bc47354d8a10a50d8e1eb3f8db4c1d2742fe9f4f
Aryann15 <rajlysm15@gmail.com>
2023-07-11 16:56:24 +0530

    minor tuning for accuracy

--- main.py
+++ main.py
@@ -7,13 +7,14 @@ img = cv.imread('test/img1.jpg')
 reader = easyocr.Reader(['en'], gpu=False)
 
 text = reader.readtext(img)
-
+x =0.25
 for t in text:
     print(t)
     bbox, text, score =t
 
-    cv.rectangle(img, bbox[0], bbox[2],(0,0,255),2)
-    cv.putText(img,text, bbox[0], cv.FONT_HERSHEY_SIMPLEX, 0.7, (0,0,255),2 )
+    if score > x:
+        cv.rectangle(img, bbox[0], bbox[2],(0,0,255),2)
+        cv.putText(img,text, bbox[0], cv.FONT_HERSHEY_SIMPLEX, 0.7, (0,0,255),2 )
 
 
 plt.imshow(cv.cvtColor(img, cv.COLOR_BGR2RGB))
