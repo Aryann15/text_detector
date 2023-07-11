7ca633e9d12e562fe0983b8f375c1646ce69c5b8
Aryann15 <rajlysm15@gmail.com>
2023-07-11 16:28:39 +0530

    detection

--- main.py
+++ main.py
@@ -6,3 +6,13 @@ img = cv.imread('test/img2.jpg')
 
 reader = easyocr.Reader(['en'], gpu=False)
 
+text = reader.readtext(img)
+
+for t in text:
+    print(t)
+    bbox, text, score =t
+
+    cv.rectangle(img, bbox[0], bbox[2],(0,0,255),5)
+
+plt.imshow(cv.cvtColor(img, cv.COLOR_BGR2RGB))
+plt.show()
